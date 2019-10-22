f48509f501ec97dbcb09ece42f4fa1177076530d
Nero <xduanshan@126.com>
2019-10-22 14:08:13 +0800

    add readme

--- src/test/java/com/duanss/redislock/RedisLockApplicationTests.java
+++ src/test/java/com/duanss/redislock/RedisLockApplicationTests.java
@@ -5,22 +5,33 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.UUID;
-
 @SpringBootTest
 class RedisLockApplicationTests {
 
     @Autowired
     RedisLock redisLock;
 
+    /** 
+    * @Description: 加锁测试 
+    * @Param: [] 
+    * @return: void 
+    * @Author: 段闪闪 duanss 
+    * @Date: 2019/10/22 14:00
+    */ 
     @Test
     void contextLoads() {
-
         String dssTEST = redisLock.tryLock("dssTEST2", 20000L);
         System.out.println(dssTEST);
 
     }
 
+    /** 
+    * @Description: 解锁测试 
+    * @Param: [] 
+    * @return: void 
+    * @Author: 段闪闪 duanss 
+    * @Date: 2019/10/22 14:01
+    */ 
     @Test
     void Test2() {
         boolean dssTEST2 = redisLock.unlock("dssTEST2", "67cb0a7d-672a-432c-abad-dfce0241f175");
